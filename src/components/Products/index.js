7b647d33bb222066d43728b83dcd63d572be771b
siri <madhurisireesha6@gmail.com>
2023-11-14 11:09:43 +0530

    Added

---
+++ src/components/Products/index.js
@@ -0,0 +1,11 @@
+import Productslist from "../Productslist"
+import Primedeals from "../Primedeals"
+const Products=()=>{
+    return(
+        <>
+            <Primedeals/>
+            <Productslist/>
+        </>
+    )
+}
+export default Products
